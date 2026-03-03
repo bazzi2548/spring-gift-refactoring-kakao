27b8e284b423cb14b26984995b3048c7993884dc
malang.cow <bazzi2548@naver.com>
2026-03-03 11:27:27 +0900

    refactor: 테스트 코드 Member 생성 중복 제거

--- src/test/java/gift/member/MemberServiceTest.java
+++ src/test/java/gift/member/MemberServiceTest.java
@@ -1,7 +1,11 @@
 package gift.member;
 
-import gift.auth.JwtProvider;
-import gift.auth.TokenResponse;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,13 +15,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.BDDMockito.given;
+import gift.auth.JwtProvider;
+import gift.auth.TokenResponse;
 
 @ExtendWith(MockitoExtension.class)
 class MemberServiceTest {
@@ -46,11 +45,15 @@ class MemberServiceTest {
     @InjectMocks
     private MemberService memberService;
 
+    private Member createMember(String email, String password) {
+        return new Member(email, new Password(password, NO_OP_ENCODER));
+    }
+
     @Test
     @DisplayName("회원 가입에 성공하면 토큰을 반환한다")
     void registerSuccess() {
         MemberRequest request = new MemberRequest("test@email.com", "password");
-        Member member = new Member("test@email.com", new Password("hashed", NO_OP_ENCODER));
+        Member member = createMember("test@email.com", "hashed");
 
         given(passwordEncoder.encode("password")).willReturn("hashed");
         given(memberRepository.existsByEmail("test@email.com")).willReturn(false);
@@ -78,7 +81,7 @@ class MemberServiceTest {
     @DisplayName("올바른 정보로 로그인하면 토큰을 반환한다")
     void loginSuccess() {
         MemberRequest request = new MemberRequest("test@email.com", "password");
-        Member member = new Member("test@email.com", new Password("hashed", NO_OP_ENCODER));
+        Member member = createMember("test@email.com", "hashed");
 
         given(memberRepository.findByEmail("test@email.com")).willReturn(Optional.of(member));
         given(passwordEncoder.matches("password", "hashed")).willReturn(true);
@@ -105,7 +108,7 @@ class MemberServiceTest {
     @DisplayName("비밀번호가 틀리면 예외가 발생한다")
     void loginWrongPassword() {
         MemberRequest request = new MemberRequest("test@email.com", "wrong");
-        Member member = new Member("test@email.com", new Password("hashed", NO_OP_ENCODER));
+        Member member = createMember("test@email.com", "hashed");
 
         given(memberRepository.findByEmail("test@email.com")).willReturn(Optional.of(member));
         given(passwordEncoder.matches("wrong", "hashed")).willReturn(false);
--- src/test/java/gift/member/MemberTest.java
+++ src/test/java/gift/member/MemberTest.java
@@ -21,10 +21,14 @@ class MemberTest {
         }
     };
 
+    private Member createMember(String email, String password) {
+        return new Member(email, new Password(password, NO_OP_ENCODER));
+    }
+
     @Test
     @DisplayName("이메일과 비밀번호로 회원을 생성한다")
     void createWithEmailAndPassword() {
-        Member member = new Member("test@email.com", new Password("password", NO_OP_ENCODER));
+        Member member = createMember("test@email.com", "password");
 
         assertThat(member.getEmail()).isEqualTo("test@email.com");
         assertThat(member.getPassword()).isEqualTo("password");
@@ -43,7 +47,7 @@ class MemberTest {
     @Test
     @DisplayName("회원 정보를 수정한다")
     void update() {
-        Member member = new Member("old@email.com", new Password("oldpass", NO_OP_ENCODER));
+        Member member = createMember("old@email.com", "oldpass");
 
         member.update("new@email.com", new Password("newpass", NO_OP_ENCODER));
 
@@ -54,7 +58,7 @@ class MemberTest {
     @Test
     @DisplayName("카카오 액세스 토큰을 갱신한다")
     void updateKakaoAccessToken() {
-        Member member = new Member("test@email.com", new Password("password", NO_OP_ENCODER));
+        Member member = createMember("test@email.com", "password");
 
         member.updateKakaoAccessToken("kakao-token-123");
 
@@ -64,7 +68,7 @@ class MemberTest {
     @Test
     @DisplayName("포인트를 충전한다")
     void chargePoint() {
-        Member member = new Member("test@email.com", new Password("password", NO_OP_ENCODER));
+        Member member = createMember("test@email.com", "password");
 
         member.chargePoint(1000);
 
@@ -74,7 +78,7 @@ class MemberTest {
     @Test
     @DisplayName("포인트를 여러 번 충전하면 누적된다")
     void chargePointAccumulates() {
-        Member member = new Member("test@email.com", new Password("password", NO_OP_ENCODER));
+        Member member = createMember("test@email.com", "password");
 
         member.chargePoint(1000);
         member.chargePoint(500);
@@ -85,7 +89,7 @@ class MemberTest {
     @Test
     @DisplayName("0 이하 금액 충전 시 예외가 발생한다")
     void chargePointWithZeroOrNegativeThrows() {
-        Member member = new Member("test@email.com", new Password("password", NO_OP_ENCODER));
+        Member member = createMember("test@email.com", "password");
 
         assertThatThrownBy(() -> member.chargePoint(0))
             .isInstanceOf(IllegalArgumentException.class);
@@ -96,7 +100,7 @@ class MemberTest {
     @Test
     @DisplayName("포인트를 차감한다")
     void deductPoint() {
-        Member member = new Member("test@email.com", new Password("password", NO_OP_ENCODER));
+        Member member = createMember("test@email.com", "password");
         member.chargePoint(1000);
 
         member.deductPoint(300);
@@ -107,7 +111,7 @@ class MemberTest {
     @Test
     @DisplayName("보유 포인트보다 많은 금액 차감 시 예외가 발생한다")
     void deductPointExceedingBalanceThrows() {
-        Member member = new Member("test@email.com", new Password("password", NO_OP_ENCODER));
+        Member member = createMember("test@email.com", "password");
         member.chargePoint(100);
 
         assertThatThrownBy(() -> member.deductPoint(200))
@@ -118,7 +122,7 @@ class MemberTest {
     @Test
     @DisplayName("0 이하 금액 차감 시 예외가 발생한다")
     void deductPointWithZeroOrNegativeThrows() {
-        Member member = new Member("test@email.com", new Password("password", NO_OP_ENCODER));
+        Member member = createMember("test@email.com", "password");
 
         assertThatThrownBy(() -> member.deductPoint(0))
             .isInstanceOf(IllegalArgumentException.class);
